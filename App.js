cbac2271aad82c090ae7fedecd41b0a0b39cb61d
javieraf2002@gmail.com <javieraf2002@gmail.com>
2023-02-22 01:19:31 -0300

    Clase Tercera

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Taller de React Native </Text>
-      <StatusBar style="auto" />
+  return(
+    <View style={styles.ST_container}>
+      {/* TITULO DE LA PANTALLA */}
+      <Text style={{
+        paddingTop:40,
+      }} 
+      >Lista de Items</Text>
+      
+      {/* INGRESO DE LOS ITEMS */}
+      <View style={styles.ST_inputContainer}>
+        <TextInput placeholder='Item de la lista' style={styles.ST_textInput} />
+        <Button title='Agregar' />
+      </View>
+
+      {/* LISTA DE ITEMS */}
+      <View style={styles.ST_itemsContainer}>
+        <Text style={styles.ST_Text}>Item 1.</Text>
+        <Text style={styles.ST_Text}>Item 2.</Text>
+        <Text style={styles.ST_Text}>Item 3.</Text>
+      </View>
+
     </View>
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  ST_container:{
+    padding:30,
   },
-});
+  ST_inputContainer:{
+    marginTop:30,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+  ST_textInput:{
+    borderBottomColor:'black',
+    borderBottomWidth: 1,
+    height:50,
+    width:200,
+  },
+  ST_itemsContainer:{
+    marginTop:50,
+  },
+  ST_Text:{
+    padding: 10,
+    textAlign: 'center',
+    color: 'green',
+  },
+})
+
